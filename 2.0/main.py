63452b0d62060b704487287ac7d61982ab426da6
threadscraper <threadscraper@protonmail.com>
2019-08-28 13:36:54 +0200

    Let URL and dest be positional, add error handling, determine platform.

--- 2.0/main.py
+++ 2.0/main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
-
+import sys
+import os
 import argparse
+import platform
 
 def main():
     parser = argparse.ArgumentParser()
 
     # parser.add_argument('', help='', type=str)
-    parser.add_argument('-u', '--url', help='URL to the 4chan thread you want to scrape', type=str)
-    parser.add_argument('-q', '--quiet', help='Run the script in quiet mode, no outputs', action='store_true')
-    parser.add_argument('-v', '--verbose', help='Run with increased verbosity', action='store_true')
-    parser.add_argument('-w', '--watch', help='Watch the thread, will check thread every 5 minutes for new posts until thread 404s', action='store_true')
+    parser.add_argument('-q', '--quiet', help='Run the script in quiet mode, no outputs', action='store_true', default=False)
+    parser.add_argument('-v', '--verbose', help='Run with increased verbosity', action='store_true', default=False)
+    parser.add_argument('-w', '--watch', help='Watch the thread, will check thread every 5 minutes for new posts until thread 404s', action='store_true', default=False)
+    parser.add_argument('url', help='URL to the 4chan thread you want to scrape', type=str)
+    parser.add_argument('destination', help='Destination folder in your home folder', type=str)
 
     args = parser.parse_args()
+
+    if args.url is None:
+        print('URL is missing, exiting')
+        sys.exit(1)
 
     # Set behaviour:
     verbose = args.verbose
@@ -20,10 +27,29 @@ def main():
 
     # Set variables
     link = args.url
+    destination = args.destination
     board = link.split('/')[3]    # 'tv', 'wg', or similar
     thread_id = link.split('/')[5]    # '114804039' or similar
     url = f'https://a.4cdn.org/{board}/thread/{thread_id}.json'
     content_url = f'https://i.4cdn.org/{board}/'
+
+
+    # Determine platform/OS and set appropriate path
+    if not destination:
+        if not quiet:
+            print('No destination folder given, exiting')
+        sys.exit(2)
+    else:
+        system = platform.system()
+        if system == 'Linux':
+            home = os.environ['HOME']
+        elif system == 'Windows':
+            home == os.environ['HOMEPATH']
+        else:
+            if not quiet:
+                print('Unsupported system, exiting')
+            sys.exit(3)
+        destination = f'{home}/{destination}'
 
     if verbose:
         print('Will scrape using the following information:')
@@ -32,6 +58,7 @@ def main():
         print(f'\tThread ID: {thread_id}')
         print(f'\tURL: {url}')
         print(f'\tContent URL: {content_url}')
+        print(f'\tYou will find the pictures in the following folder: {destination}')
 
 if __name__ == '__main__':
     main()
